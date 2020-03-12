d8d3ef6a20e8e841de3af4fa066f8b446a97df3e
Matt Huston <matthuston1@aol.com>
2020-03-12 12:33:24 -0600

    got basic stock info to pull up when typing in stock symbol

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var symbolSearch = document.querySelector('.symbolSearch')
 var symbolName = document.querySelector('.symbolName')
 var curPrice = document.querySelector('.curPrice')
 var change = document.querySelector('.change')
+var changePer = document.querySelector('.changePer')
+var open = document.querySelector('.open')
+var high = document.querySelector('.high')
+var low = document.querySelector('.low')
+var preClo = document.querySelector('.preClo')
+var volume = document.querySelector('.volume')
+
+
 
 var symbol = ''
 
@@ -35,6 +43,12 @@ function stockInfo(myJSON) {
   var gloQuo = myJSON['Global Quote']
   symbolName.textContent = gloQuo['01. symbol']
   curPrice.textContent = 'Price: ' + gloQuo['05. price']
-  change.textContent = 'Change: ' + gloQuo['09. change'] + '%'
+  change.textContent = 'Change: $' + gloQuo['09. change']
+  changePer.textContent = 'Change: ' + gloQuo['10. change percent']
+  open.textContent = 'Open: $' + gloQuo['02. open']
+  high.textContent = 'High: $' + gloQuo['03. high']
+  low.textContent = 'Low: $' + gloQuo['04. low']
+  preClo.textContent = 'Previous Close: $' + gloQuo['08. previous close']
+  volume.textContent = 'Volume: ' + gloQuo['06. volume']
 
 }
